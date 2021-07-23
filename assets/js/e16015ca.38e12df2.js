e097ed8c71dcfb33b38e08e9ba8565959bd99e4f
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2021-07-23 20:34:11 +0700

    Deploy website - based on 7766a8b88606791fd616dba723876f2d7a013e83

---
+++ assets/js/e16015ca.38e12df2.js
@@ -0,0 +1,1 @@
+(self.webpackChunksuperyou_partnership_wiki=self.webpackChunksuperyou_partnership_wiki||[]).push([[700],{947:function(a){"use strict";a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
